069f719aa5e370ed3baeaac1a718c6004d78aff7
unknown <anhocva214>
2020-12-17 10:02:46 +0700

    17/12/2020

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
         loop: true,
         margin: 10,
         nav: true,
+        navText: ["<i class=\"fa fa-chevron-left\" aria-hidden=\"true\"></i>","<i class=\"fa fa-chevron-right\" aria-hidden=\"true\"></i>"],
         responsive: {
             0: {
                 items: 1,
@@ -16,6 +17,24 @@ $(document).ready(function () {
             },
             1000: {
                 items: 1,
+            }
+        }
+    })
+});
+
+$(document).ready(function () {
+    $('.owl-carousel-products').owlCarousel({
+        loop: true,
+        margin: 10,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 3,
+            },
+            1000: {
+                items: 4,
             }
         }
     })
